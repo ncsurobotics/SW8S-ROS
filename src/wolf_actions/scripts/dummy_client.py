5e3877aef91b5d262a2c1e31ff05332cc5a2f597
Amr Moussa <amrmoussa100@gmail.com>
2020-05-14 01:10:21 -0400

    Added IMU Euler angle publication to pixhawk, updated README, added launch file, formatting fixes

--- src/wolf_actions/scripts/dummy_client.py
+++ src/wolf_actions/scripts/dummy_client.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import roslib
+
 roslib.load_manifest('wolf_actions')
 import rospy
 import actionlib
@@ -8,20 +9,20 @@ import actionlib
 from wolf_actions.msg import submerge_secondsAction, submerge_secondsGoal, drive_secondsAction, drive_secondsGoal
 
 if __name__ == '__main__':
-	rospy.init_node('dummy_client')
+    rospy.init_node('dummy_client')
 
-	client = actionlib.SimpleActionClient('submerge_seconds', submerge_secondsAction)
-	client.wait_for_server()
+    client = actionlib.SimpleActionClient('submerge_seconds', submerge_secondsAction)
+    client.wait_for_server()
 
-	goal = submerge_secondsGoal()
-	goal.seconds_goal = 5.0
-	client.send_goal(goal)
-	client.wait_for_result(rospy.Duration.from_sec(10.0))
+    goal = submerge_secondsGoal()
+    goal.seconds_goal = 5.0
+    client.send_goal(goal)
+    client.wait_for_result(rospy.Duration.from_sec(10.0))
 
-	client = actionlib.SimpleActionClient('drive_seconds', drive_secondsAction)
-	client.wait_for_server()
+    client = actionlib.SimpleActionClient('drive_seconds', drive_secondsAction)
+    client.wait_for_server()
 
-	goal = drive_secondsGoal()
-	goal.seconds_goal = 10.0
-	client.send_goal(goal)
-	client.wait_for_result(rospy.Duration.from_sec(15.0))
+    goal = drive_secondsGoal()
+    goal.seconds_goal = 10.0
+    client.send_goal(goal)
+    client.wait_for_result(rospy.Duration.from_sec(15.0))
--- src/wolf_serial/scripts/pixhawk.py
+++ src/wolf_serial/scripts/pixhawk.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
+import math
 import rospy
 import mavros
 from std_msgs.msg import Float64
+from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import Imu
 from mavros_msgs.msg import OverrideRCIn
+
+
+def quaternion_to_euler(x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    X = math.degrees(math.atan2(t0, t1))
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    Y = math.degrees(math.asin(t2))
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    Z = math.degrees(math.atan2(t3, t4))
+
+    return X, Y, Z
 
 
 def value_map(value, istart, istop, ostart, ostop):
@@ -23,22 +43,31 @@ class Pixhawk:
     def rotation_callback(self, data):
         self.rotation_rate = value_map(data.data, -1.0, 1.0, 1000, 2000)
 
+    def imu_callback(self, data):
+        euler = Vector3()
+        euler.x, euler.y, euler.z = quaternion_to_euler(data.orientation.x, data.orientation.y, data.orientation.z,
+                                                        data.orientation.w)
+        self.imu_pub.publish(euler)
+
     def __init__(self):
         rospy.init_node('pixhawk', anonymous=False)
         rate = rospy.Rate(20)
         mavros.set_namespace()
 
+        rc = OverrideRCIn()
+        self.override_pub = rospy.Publisher(mavros.get_topic("rc", "override"), OverrideRCIn, queue_size=10)
+        self.imu_pub = rospy.Publisher("wolf_imu_euler", Vector3, queue_size=10)
+
         rospy.Subscriber("wolf_vertical", Float64, self.vertical_callback)
         rospy.Subscriber("wolf_lateral", Float64, self.lateral_callback)
         rospy.Subscriber("wolf_rotation", Float64, self.rotation_callback)
+        rospy.Subscriber("mavros/imu/data", Imu, self.imu_callback)
 
-        rc = OverrideRCIn()
-        override_pub = rospy.Publisher(mavros.get_topic("rc", "override"), OverrideRCIn, queue_size=10)
         while not rospy.is_shutdown():
             rc.channels[2] = self.vertical_rate
             rc.channels[3] = self.rotation_rate
             rc.channels[4] = self.lateral_rate
-            override_pub.publish(rc)
+            self.override_pub.publish(rc)
             rate.sleep()
 
 
