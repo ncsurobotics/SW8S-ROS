7e98c497ff3e9a878117f1547e25f7cc5d23dfd6
Henry Sneed <hjsneed@ncsu.edu>
2020-07-17 16:41:39 -0400

    slight style edits to meb.py

--- src/wolf_serial/scripts/meb.py
+++ src/wolf_serial/scripts/meb.py
@@ -3,13 +3,16 @@ import rospy
 import serial
 import time
 from std_msgs.msg import String   
+from std_msgs.msg import Float64
 
 ser = serial.Serial('/dev/ttyACM0')
 
 def display():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('wolf_meb', Float64, queue_size=10)
     rospy.init_node('meb', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+ 
+    rate = rospy.Rate(1) # 10hz
+  
     while not rospy.is_shutdown():
         print(ser.name)
         print(ser.readline())
@@ -25,17 +28,21 @@ def display():
         #Address
         ser.write('T')
         print(ser.readline())
-        
+          
         while True:
-            while ser.readline()[0] == "9":
-                depthString = ser.readline()[2:6]
-                killString = ser.readline()[6]
-                print("depth: " + depthString)
-                if killString == "0":
-                    killStatus = "operating"
-                else: killStatus = "killed"
-                print("kill status: " + killStatus)           
-        rate.sleep()
+            pub.publish(1.0)
+            rate.sleep()
+           # while ser.readline()[0] == "9":
+            #  depthString = ser.readline()[2:6]
+             # killString = ser.readline()[6]
+              #print("depth: " + depthString)
+              #if killString == "0":
+               # killStatus = "operating"
+                #pub.publish(1.0)
+              #else: 
+               # killStatus = "killed"
+                #pub.publish(0.0)
+              #print("kill status: " + killStatus) 
 
 if __name__ == '__main__':
         display()
